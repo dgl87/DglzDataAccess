2811a755032830cadbb07f5c1f0d9d31ba17de18
Douglas <dglandre@gmail.com>
2022-12-21 18:11:15 -0300

    feat: Dapper - Transactions

--- DglZDataAccess/Program.cs
+++ DglZDataAccess/Program.cs
@@ -28,7 +28,8 @@ namespace DglZDataAccess
                 //OneToMany(connection);
                 //QueryMultiple(connection);
                 //SelectIn(connection);
-                Like(connection, "api");
+                //Like(connection, "api");
+                Transaction(connection);
             }
         }
 
@@ -340,6 +341,46 @@ namespace DglZDataAccess
             foreach (var item in items)
             {
                 Console.WriteLine(item.Title);
+            }
+        }
+        static void Transaction(SqlConnection connection)
+        {
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "Não quero salvar";
+            category.Url = "amazon";
+            category.Description = "Categoria destinada a serviços do AWS";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+
+            //SQL Injection
+            var insertSql = @"INSERT INTO 
+                                    [Category] 
+                                VALUES(
+                                    @Id, 
+                                    @Title, 
+                                    @Url, 
+                                    @Summary, 
+                                    @Order, 
+                                    @Description, 
+                                    @Featured)";
+            connection.Open();
+            using (var transaction = connection.BeginTransaction())
+            {
+                var rows = connection.Execute(insertSql, new
+                {
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured,
+                }, transaction);
+                //transaction.Commit();
+                transaction.Rollback();
+                Console.WriteLine($"{rows} linhas inseridas");
             }
         }
     }
