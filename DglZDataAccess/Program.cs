5c6b05eb2f79a60c29eafce4b08e66d09685cfdc
Douglas <dglandre@gmail.com>
2022-12-21 12:16:24 -0300

    feat: Add List, Create and Update

--- DglZDataAccess/Program.cs
+++ DglZDataAccess/Program.cs
@@ -10,7 +10,25 @@ namespace DglZDataAccess
         static void Main(string[] args)
         {
             const string connectionString = "Server=localhost,1433;Database=DglZ;User ID=sa;Password=2w3e4r5t!@#;TrustServerCertificate=true";
+            
+            using (var connection = new SqlConnection(connectionString))
+            {
+                UpdateCategory(connection);
+                ListCategories(connection);
+                CreateCategory(connection);
+            }
+        }
 
+        static void ListCategories(SqlConnection connection)
+        {
+            var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+            foreach (var item in categories)
+            {
+                Console.WriteLine($"{item.Id} - {item.Title}");
+            }
+        }
+        static void CreateCategory(SqlConnection connection)
+        {
             var category = new Category()
             {
                 Id = Guid.NewGuid(),
@@ -33,26 +51,29 @@ namespace DglZDataAccess
                                 @Description, 
                                 @Featured)";
 
-            using (var connection = new SqlConnection(connectionString))
+            var rows = connection.Execute(insertSql, new
             {
-                var rows = connection.Execute(insertSql, new
-                {
-                    category.Id,
-                    category.Title,
-                    category.Url,
-                    category.Summary,
-                    category.Order,
-                    category.Description,
-                    category.Featured
-                });
-                Console.WriteLine($"{rows} linhas inseridas");
+                category.Id,
+                category.Title,
+                category.Url,
+                category.Summary,
+                category.Order,
+                category.Description,
+                category.Featured
+            });
 
-                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-                foreach (var item in categories)
-                {
-                    Console.WriteLine($"{item.Id} - {item.Title}");
-                }
-            }
+            Console.WriteLine($"{rows} linhas inseridas");
+        }
+
+        static void UpdateCategory(SqlConnection connection)
+        {
+            var updateQuery = "UPDATE [Category] SET [Title]=@title WHERE [Id]=@id";
+            var rows = connection.Execute(updateQuery, new
+            {
+                Id = new Guid("af3407aa-11ae-4621-a2ef-2028b85507c4"),
+                Title = "Frontend 2021"
+            });
+            Console.WriteLine($"{rows} registros atualizadas");
         }
     }
 }
