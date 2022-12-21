df41583908ea547b2b43e7f641eb5b622fb7e192
Douglas <dglandre@gmail.com>
2022-12-21 12:00:17 -0300

    feat: Dapper - INSERT

--- DglZDataAccess/Program.cs
+++ DglZDataAccess/Program.cs
@@ -11,12 +11,46 @@ namespace DglZDataAccess
         {
             const string connectionString = "Server=localhost,1433;Database=DglZ;User ID=sa;Password=2w3e4r5t!@#;TrustServerCertificate=true";
 
+            var category = new Category()
+            {
+                Id = Guid.NewGuid(),
+                Title = "Amazon AWS",
+                Url = "amazon",
+                Description = "Categoria destinada a serviços do AWS",
+                Order = 8,
+                Summary = "AWS Cloud",
+                Featured = false
+            };
+
+            var insertSql = @"INSERT INTO 
+                                [Category] 
+                            VALUES (
+                                @Id, 
+                                @Title, 
+                                @Url, 
+                                @Summary, 
+                                @Order, 
+                                @Description, 
+                                @Featured)";
+
             using (var connection = new SqlConnection(connectionString))
             {
-                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-                foreach (var category in categories)
+                var rows = connection.Execute(insertSql, new
                 {
-                    Console.WriteLine($"{category.Id} - {category.Title}");
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured
+                });
+                Console.WriteLine($"{rows} linhas inseridas");
+
+                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+                foreach (var item in categories)
+                {
+                    Console.WriteLine($"{item.Id} - {item.Title}");
                 }
             }
         }
