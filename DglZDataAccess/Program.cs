543fd7ea4e5d13badd7cd22fc4d017e97ec1de82
Douglas <dglandre@gmail.com>
2022-12-21 11:21:12 -0300

    feat: ADO.NET

---
+++ DglZDataAccess/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using System;
+
+namespace DglZDataAccess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Server=localhost,1433;Database=DglZ;User ID=sa;Password=2w3e4r5t!@#;TrustServerCertificate=true";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+                    }
+                }
+            }
+        }
+    }
+}
